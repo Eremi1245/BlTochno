ffb49b011f00cadd4921dffeb2148e1a789f6713
Eremi1245 <xxxmessixxx@mail.ru>
2022-09-03 16:38:29 +0300

    create Category Events Habit Habits_Events tables

--- DataBase/models.py
+++ DataBase/models.py
@@ -1,14 +1,36 @@
-from sqlalchemy import Column, Integer, String, create_engine, DateTime, ForeignKey, VARCHAR
+from sqlalchemy import Column, Integer, String, create_engine, DateTime, ForeignKey, VARCHAR, Date, Time
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 from db_setup import Base
 
 
+class Category(Base):
+    __tablename__ = 'categories' #название таблицы
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name=Column(String(120))
+
 class Events(Base):
     __tablename__ = 'events' #название таблицы
     id = Column(Integer, primary_key=True, autoincrement=True)
+    category=Column(Integer,ForeignKey("categories.id"))
     name = Column(String(120))
+    dt=Column(Date)
+    tm=Column(Time)
     desc = Column(String(120))
+
+class Habit(Base):
+    __tablename__ = 'habits' #название таблицы
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    category = Column(Integer, ForeignKey("categories.id"))
+    name=Column(String(120))
+
+class Habits_Events(Base):
+    __tablename__ = 'habits_events' #название таблицы
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    habit_id=Column(Integer,ForeignKey("habits.id"))
+    event_id = Column(Integer, ForeignKey("events.id"))
+
+
 
 
