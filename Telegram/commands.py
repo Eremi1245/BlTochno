26d8570512718fdc10144941052d041b2353a4be
Eremi1245 <xxxmessixxx@mail.ru>
2022-09-08 01:34:10 +0300

    функция добавления ивента,календарь и просмотр задач на сегодня

--- Telegram/commands.py
+++ Telegram/commands.py
@@ -39,57 +39,13 @@ def get_object(desired_object,**kwargs):
             print(er)
 
 
-# class Add_Object_Controller(metaclass=Singleton):
-#
-#
-#     def __init__(self,obj):
-#         self.obj=obj
-#         self.attrs={}
-#         self.get_obj_attrs()
-#
-#
-#     def get_obj_attrs(self):
-#         fields=deepcopy(self.obj.__dict__['_sa_instance_state']['unloaded'])
-#         values=[None*len(fields)]
-#         self.attrs=dict(zip(fields,values))
-#         try:
-#             del self.attrs['id']
-#         except Exception as er:
-#             print(f'В классе {self.obj.__tablename__} нет атрибута id!!!')
-#
-#     def start(self,messages,name_value_attributes:dict):
-#         attrib_name=name_value_attributes['attrib_name']
-#         attrib_value=name_value_attributes['attrib_value']
-#         for atrib,value in self.attrs.items():
-#             if not value:
-#                 name_value_attributes.update({'attrib_name': atrib})
-#                 bot.send_message(message.from_user.id, 'Введите название евента')
-#                 bot.register_next_step_handler(message, add_event, args)
-#         elif not new_event.dt:
-#             args.update({'attribute': 'dt'})
-#             bot.send_message(message.from_user.id, 'Введите Дату ивента')
-#             bot.register_next_step_handler(message, add_event, args)
-#         elif not new_event.tm:
-#             args.update({'attribute': 'tm'})
-#             bot.send_message(message.from_user.id, 'Введите Время ивента')
-#             bot.register_next_step_handler(message, add_event, args)
-#         elif not new_event.category:
-#             args.update({'attribute': 'category'})
-#             categories = get_object(Category)
-#             keyboard = types.InlineKeyboardMarkup()
-#             for categ in categories:
-#                 key = types.InlineKeyboardButton(text=categ.name, callback_data=categ.id)
-#                 keyboard.add(key)
-#             question = 'Выбери id категории ивента'
-#             bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-#         elif not new_event.desc:
-#             args.update({'attribute': 'desc'})
-#             bot.send_message(message.from_user.id, 'Введите описание ивента\nВведите "нет" если описание не требуется')
-#             bot.register_next_step_handler(message, add_event, args)
-#         else:
-#             result = add_to_data(new_event)
-#             if result["status"]:
-#                 bot.send_message(message.from_user.id, 'Евент добавлен')
-#             else:
-#                 bot.send_message(message.from_user.id, f'Ошибка: {result["error"]}')
+def my_day(desired_object):
+    today=datetime.today().date()
+    try:
+        result = select(desired_object).where(desired_object.dt==today).order_by(desired_object.tm)
+        result = session.execute(result) # а это уже ее непосредственное
+        result=[res[0] for res in result]
+        return result
+    except Exception as er:
+        print(er)
 
--- Telegram/main.py
+++ Telegram/main.py
@@ -1,11 +1,11 @@
 from copy import deepcopy
 from datetime import datetime
-
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 from telebot import TeleBot, types
 
 from BlTochno.DataBase.models import Event, Category
 from BlTochno.DataBase.session import Sessn
-from BlTochno.Telegram.commands import add_to_data, get_object
+from BlTochno.Telegram.commands import add_to_data, get_object, my_day
 from BlTochno.const import default_response
 from BlTochno.other.supportive_classes import Singleton
 from secret import TOKEN
@@ -27,7 +27,9 @@ def bot_start(message):
 
 @bot.message_handler(commands=['my_day'])
 def get_my_day(message):
-    bot.send_message(message.from_user.id, 'Расписание на день')
+    ma_day=my_day(Event)
+    answer='\n'.join([f'{i+1}.{ma_day[i].name} ({ma_day[i].desc}) - {ma_day[i].tm}' for i in range(len(ma_day))])
+    bot.send_message(message.from_user.id, answer)
 
 @bot.message_handler(commands=['my_week'])
 def get_my_week(message):
@@ -41,52 +43,18 @@ def add_event(message):
     event=Event()
     bot.send_message(message.from_user.id, 'Введите название евента')
     bot.register_next_step_handler(message, event_name)
-    # elif not new_event.dt:
-    #     data.update({'attribute': 'dt'})
-    #     bot.send_message(message.from_user.id, 'Введите Дату ивента')
-    #     bot.register_next_step_handler(message,enter_date)
-    # elif not new_event.tm:
-    #     data.update({'attribute': 'tm'})
-    #     bot.send_message(message.from_user.id, 'Введите Время ивента')
-    #     bot.register_next_step_handler(message, add_event, data)
-    # elif not new_event.category:
-    #     data.update({'attribute': 'category'})
-    #     categories=get_object(Category)
-    #     keyboard = types.InlineKeyboardMarkup()
-    #     for categ in categories:
-    #         key = types.InlineKeyboardButton(text=categ.name, callback_data=categ.id)
-    #         keyboard.add(key)
-    #     question = 'Выбери id категории ивента'
-    #     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-    # elif not new_event.desc:
-    #     data.update({'attribute': 'desc'})
-    #     bot.send_message(message.from_user.id, 'Введите описание ивента\nВведите "нет" если описание не требуется')
-    #     bot.register_next_step_handler(message, add_event, data)
-    # else:
-    #     result=add_to_data(new_event)
-    #     if result["status"]:
-    #         bot.send_message(message.from_user.id, 'Евент добавлен')
-    #     else:
-    #         bot.send_message(message.from_user.id, f'Ошибка: {result["error"]}')
 
 @bot.message_handler(commands=['event_name'])
 def event_name(message):
     name=message.text
     event.name=name
     bot.send_message(message.from_user.id, 'Введите дату ивента')
-    bot.register_next_step_handler(message, event_date)
+    calendar(message)
 
-@bot.message_handler(commands=['event_date'])
-def event_date(message):
-    dt=message.text
-    dt=datetime.strptime(dt, "%Y-%m-%d").date()
-    event.dt=dt
-    bot.send_message(message.from_user.id, 'Введите время ивента')
-    bot.register_next_step_handler(message, event_time)
 
 @bot.message_handler(commands=['event_time'])
 def event_time(message):
-    tm=message.text
+    tm=message.text + ':00'
     tm=datetime.strptime(tm, "%H:%M:%S").time()
     event.tm=tm
     categories = get_object(Category)
@@ -99,11 +67,6 @@ def event_time(message):
     bot.register_next_step_handler(message, event_desc)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    id=call.data
-    # category=get_object(Category,id=id)[0]
-    event.category=id
 
 @bot.message_handler(commands=['event_desc'])
 def event_desc(message):
@@ -116,48 +79,35 @@ def event_desc(message):
         bot.send_message(message.from_user.id, f'Ошибка: {result["error"]}')
 
 
-@bot.message_handler(commands=['add_category'])
-def add_category(message,args:dict=None):
-    if not args:
-        args={'new_category':Category(),'attribute':''}
-    new_object=args['new_category']
-    attrib = args['attribute']
-    if attrib and message.text!='/add_category':
-        setattr(new_object, attrib, message.text)
+@bot.message_handler(commands=['calendar'])
+def calendar(m):
+    calendar, step = DetailedTelegramCalendar(calendar_id=1).build()
+    bot.send_message(m.chat.id,
+                     f"Select {LSTEP[step]}",
+                     reply_markup=calendar)
 
-    if not new_object.name:
-        args.update({'attribute':'name'})
-        bot.send_message(message.from_user.id, 'Введите название категории')
-        bot.register_next_step_handler(message,add_category,args)
-    else:
-        result=add_to_data(new_object)
-        if result["status"]:
-            bot.send_message(message.from_user.id, 'Категория добавлена')
-        else:
-            bot.send_message(message.from_user.id, f'Ошибка: {result["error"]}')
+@bot.callback_query_handler(func=DetailedTelegramCalendar.func(calendar_id=1))
+def cal(c):
+    min_date = datetime.today().date()
+    print(min_date)
+    result, key, step = DetailedTelegramCalendar(calendar_id=1,locale='ru',min_date=min_date).process(c.data)
+    if not result and key:
+        bot.edit_message_text(f"Select {LSTEP[step]}",
+                              c.message.chat.id,
+                              c.message.message_id,
+                              reply_markup=key)
+    elif result:
+        bot.edit_message_text(f"You selected {result}",
+                              c.message.chat.id,
+                              c.message.message_id)
+        event.dt = result
+        bot.send_message(c.from_user.id, 'Введите время ивента')
+        bot.register_next_step_handler(c.message, event_time)
 
-
-def get_name(message): #получаем фамилию
-    global name
-    name = message.text
-    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
-    bot.register_next_step_handler(message, get_surname)
-
-def get_surname(message):
-    global surname
-    surname = message.text
-    bot.send_message(message.from_user.id,'Сколько тебе лет?')
-    bot.register_next_step_handler(message, get_age)
-
-def get_age(message):
-
-        keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
-        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
-        keyboard.add(key_yes); #добавляем кнопку в клавиатуру
-        key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
-        keyboard.add(key_no);
-        question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    id=call.data
+    event.category=id
 
 
 
