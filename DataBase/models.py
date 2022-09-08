2975d77f2812f1ff52530fcd7a1b70a6621d2bee
“A.E.Ivanov” <a.e.ivanov@group-ib.com>
2022-09-08 18:42:27 +0300

    erwerwe

--- DataBase/models.py
+++ DataBase/models.py
@@ -35,5 +35,28 @@ class Habits_Events(Base):
     habit_id=Column(Integer,ForeignKey("habits.id"))
     event_id = Column(Integer, ForeignKey("events.id"))
 
+class HookaCategory(Base):
+    __tablename__ = 'hooka_categories' #название таблицы
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name=Column(String(120))
+
+class HookaComponent(Base):
+    __tablename__ = 'hooka_components' #название таблицы
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name=Column(String(120))
+    category=Column(Integer, ForeignKey("hooka_categories.id"))
 
 
+class HookaComponentAnalogs(Base):
+    __tablename__ = 'hooka_component_analogs' #название таблицы
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    main_component=Column(Integer, ForeignKey("hooka_components.id"))
+    analog_component=Column(Integer, ForeignKey("hooka_components.id"))
+
+
+class HookaMixRecept(Base):
+    __tablename__ = 'hooka_mix_recepts' #название таблицы
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name=Column(String(120))
+    component=Column(Integer, ForeignKey("hooka_components.id"))
+    desc=Column(String(120))
--- common_utils/parser.py
+++ common_utils/parser.py
@@ -1,6 +1,35 @@
 import re
 
 
+class NNKalyanDecorator(object):
+
+
+    def __init__(self, parser: object):
+        self.parser=parser
+
+    def __call__(self, *args, **kwds):
+        self.parser=self.parser(*args, **kwds)
+        return self
+
+    def start(self,html:list[str])->list[str]:
+        right_lst_components=[]
+        components=self.parser.start(html=html)
+        for component in components:
+            right_components=[]
+            component=component.split('+')
+            for comp in component:
+                right_component=comp.split(' ')
+                right_component=list(filter(lambda a: a != '', right_component))
+                if len(right_component)>1:
+                    right_component=' '.join(right_component)
+                else:
+                    right_component=right_component[0]
+                right_components.append(right_component.lower())
+            right_lst_components.append(right_components)
+        return right_lst_components
+
+
+@NNKalyanDecorator
 class Parser:
     """Base parser class.
     """
@@ -20,3 +49,9 @@ class Parser:
         html = html.replace('\n', '')
         result = re.findall(self.regex, html)
         return result
+
+
+
+
+
+
