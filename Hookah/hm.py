1b9bfeb46e98ebce0f9c0caa099baa5bdb1000af
“A.E.Ivanov” <a.e.ivanov@group-ib.com>
2022-09-14 17:49:58 +0300

    update hooka master

--- Hookah/hm.py
+++ Hookah/hm.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+from importlib.abc import SourceLoader
 from BlTochno.Hookah.wordanalayzer import WordsAnalayzer
 from BlTochno.common_utils.htmlcollector import HtmlCollector
 from BlTochno.common_utils.htmlparser import HtmlParser
@@ -5,72 +7,12 @@ from BlTochno.common_utils.urlobject import UrlObject
 from BlTochno.Hookah.hooka_settings import sites_with_mixes
 from collections import defaultdict
 
-storage=['грейпфрут','патока','киви','вишня','','корица','кукуруза','молоко','deep blue sea','мед','лимон','орех','raf in the jungle','мороженное','adalia power','лед','энергетик','love 66','']
-mixes={
-    1:{'main':'апельсин','supplement':'персик','shade': 'ягоды'},
-    2:{'main':'Банан','supplement':'клубника','shade': 'мята'},
-    3:{'main':"Банан",'supplement':'кокос','shade':''},
-    4:{'main':'Арбуз','supplement':'апельсин','shade': 'ваниль'},
-    5:{'main':'ваниль','supplement':'апельсин','shade': 'кокос'},
-    6:{'main':'Fresh Mist','supplement':' клубника', 'shade':'кокос','shade': 'мята'},
-    7:{'main':'Ягоды','supplement': 'арбуз', 'shade':'лимон','shade': 'мята'},
-    8:{'main':'Манго','supplement': 'мед', 'shade':'роза','shade': 'ваниль'},
-    9:{'main':'вишня','supplement': 'слива', 'shade':'гранат','shade': 'лимон'},
-    11:{'main':'Глинтвейн','supplement': 'мед', 'shade':'тархун','shade': 'базилик'},
-    12:{'main':'Апельсин', 'supplement':'персик','shade': 'ягоды'},
-    13:{'main':'Банан', 'supplement':'клубника','shade': 'мята'},
-    14:{'main':'Арбуз', 'supplement':'апельсин','shade': 'ваниль'},
-    15:{'main':'Двойное яблоко', 'supplement':'ваниль','shade': 'корица'},
-    16:{'main':'Виноград', 'supplement':'гранат','shade': 'мята'},
-    17:{'main':'Дыня', 'supplement':'вишня','shade': 'ананас'},
-    18:{'main':'Персик', 'supplement':'гуава','shade': 'виноград'},
-    19:{'main':'Лесные ягоды', 'supplement':'манго','shade': 'вишня'},
-    20:{'main':'Шоколад', 'supplement':'кокос','shade': 'ваниль'},
-    21:{'main':'киви', 'supplement':'клубника','shade': 'мята'},
-    22:{'main':'Апельсин', 'supplement':'слива','shade': 'ваниль'},
-    23:{'main':'Fresh Mint Flavour' , 'supplement':'кокос','shade': 'слива'},
-    24:{'main':'Карамель', 'supplement':'фисташка','shade': 'шоколад'},
-    25:{'main':'Тирамису', 'supplement':'орех','shade': 'чизкейк'},
-    26:{'main':'Двойное яблоко', 'supplement':'капучино','shade': 'перечная мята'},
-    27:{'main':'Ананас', 'supplement':'грейпфрут','shade': 'капучино'},
-    28:{'main':'Карамель', 'supplement':'двойное яблоко','shade': 'земляника'},
-    29:{'main':'вишня', 'supplement':'лимон','shade': 'мята'},
-    30:{'main':'Яблоко', 'supplement':'кола','shade': 'капучино'},
-    31:{'main':'Кофе', 'supplement':'мед','shade': 'ваниль'},
-    32:{'main':'Кокосовый орех', 'supplement':'кола','shade': 'жасмин'},
-    33:{'main':'Двойное яблоко', 'supplement':'персик','shade': 'фрукты'},
-    34:{'main':'Фруктовый микс' , 'supplement':'апельсин','shade': 'двойное яблоко'},
-    35:{'main':'Банан','supplement': 'кокос','shade':''},
-    36:{'main':'Карамель','supplement': 'шоколад','shade':''},
-    37:{'main':'Апельсин','supplement': 'шоколад','shade':''},
-    38:{'main':'мед','supplement': 'лесные ягоды','shade':''},
-    39:{'main':'Персик','supplement': 'жасмин','shade':''},
-    40:{'main':'Ананас','supplement': 'ежевика','shade':''},
-    41:{'main':'Кокос','supplement': 'малина','shade':''},
-    42:{'main':'Слива','supplement': 'ананас','shade':''},
-    43:{'main':'Персик','supplement': 'ежевика','shade':''},
-    44:{'main':'лимон','supplement': 'ваниль','shade':''},
-    45:{'main':'лимон','supplement': 'мята','shade':''},
-    46:{'main':'Cola','supplement': 'лимон','shade':''},
-    47:{'main':'Фруктовый микс', 'supplement': 'яблоко','shade':''},
-    48:{'main':'Абрикос','supplement': 'мята','shade':''},
-    49:{'main':'Виноград','supplement': 'банан','shade':''},
-    50:{'main':'вишня','supplement': 'перечная мята','shade':''}, 
-    51:{'main':'Двойное яблоко','supplement':'мята','shade':''},
-    52:{'main':'Cola','supplement': 'вишня','shade':''},
-    53:{'main':'Манго','supplement': 'вишня','shade':''},
-    54:{'main':'Яблоко','supplement': 'апельсин','shade':''},
-    55:{'main':'Жасмин','supplement': 'ваниль','shade':''},
-    56:{'main':'Манго','supplement': 'арбуз','shade':''},
-    57:{'main':'Персик','supplement': 'апельсин','shade':''},
-}
-
 class HookaMaster:
 
 
-    def __init__(self,word_analayzer:WordsAnalayzer=None) -> None:
-        self.storage=[]
-        self.mixes=[]
+    def __init__(self,storage:list[str],word_analayzer:WordsAnalayzer=None) -> None:
+        self.storage=storage
+        self.mixes=dict()
         self.sites_with_mixes=[]
         self.urls:list[UrlObject]=[]
         self.html_collector:HtmlCollector=HtmlCollector()
@@ -85,6 +27,21 @@ class HookaMaster:
             if all(check_ingridients):
                 print(f'Мы можем сделать микс: {v}')
     print('Приятного покура')
+
+
+
+    def urls_to_mix(self):
+        for url in self.urls:
+            for mix_index in range(len(url.regex_matchs)):
+                name_of_mix=self.mix_hash(url.regex_matchs[mix_index])
+                self.mixes[name_of_mix]=dict()
+                for component_index in range(len(url.regex_matchs[mix_index])):
+                    self.mixes[name_of_mix][f'Component #{component_index+1}']=url.regex_matchs[mix_index][component_index]
+
+    def mix_hash(self,mix_lst:list[str]):
+        mix_lst=set(mix_lst)
+        hash=md5(mix_lst)
+        return hash
 
 
     def parse_new_mixes(self)->None:
--- Hookah/wordanalayzer.py
+++ Hookah/wordanalayzer.py
@@ -17,13 +17,21 @@ class WordsAnalayzer:
     def analize(self,urls:list[UrlObject])->list:
         try:
             for url in urls:
-                for match in url.regex_matchs:
-                    for word in match:
-                        for k,v in self.word_dict.items():
-                            for v1 in v:
-                                if v1 in word:
-                                    word=k
-                                    break
+                for match_index in range(len(url.regex_matchs)):
+                    for component_index in range(len(url.regex_matchs[match_index])):
+                        word=url.regex_matchs[match_index][component_index]
+                        url.regex_matchs[match_index][component_index]=self.check_word(word)
         except Exception as er:
             print(er)
         return urls
+
+    def check_word(self,word:str):
+        for original,alternatives in self.word_dict.items():
+            if original==word:
+                return original
+            else:
+                for alternativ in alternatives:
+                    if alternativ in word:
+                        return original
+        print(f'Не нашли компонента "{word}" в словаре')
+        return word
