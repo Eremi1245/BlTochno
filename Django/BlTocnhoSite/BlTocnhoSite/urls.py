c95bcf2c18c59cb68960db757c6cbe6d08bfc147
“A.E.Ivanov” <a.e.ivanov@group-ib.com>
2022-09-19 17:52:36 +0300

    django api add

--- Django/BlTocnhoSite/BlTocnhoSite/urls.py
+++ Django/BlTocnhoSite/BlTocnhoSite/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import index
+from API.models import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('calendar/',include('events.urls')),
-    path('',index)
+    path('',index),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
