a8b63998bcb7f529fc079e63c63f19e833109069
“A.E.Ivanov” <a.e.ivanov@group-ib.com>
2022-09-22 17:51:07 +0300

    django

--- Django/BlTocnhoSite/events/views.py
+++ Django/BlTocnhoSite/events/views.py
@@ -1,6 +1,9 @@
-from datetime import date, timedelta
+from collections import defaultdict
+from datetime import date, datetime, timedelta
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from events.models import Event
 
 today=date.today()
 my_year=today.year
@@ -12,35 +15,67 @@ def calendar(request,year:int=None,month:int=None):
         year=my_year
     if not month:
         month=my_month
-    print(year)
-    print(month)
-    month_html=calendar_builder(year,month)
+    month_list=calendar_builder(year,month)
+    month_dict=days_to_events(month_list)
+    if month==12:
+        next_year=year+1
+        prev_year=year
+        next_month=1
+        prev_month=month-1
+    elif month==1:
+        prev_year=year-1
+        prev_month=12
+        next_year=year
+        next_month=month+1
+    else:
+        next_year=year
+        prev_year=year
+        next_month=month+1
+        prev_month=month-1
     context={
-        'month_list': month_html,
+        'month_list': month_dict,
         'year':year,
+        'next_year':next_year,
+        'prev_year':prev_year,
         'month':month,
-        'prev_month':month-1,
-        'next_month':month+1
+        'prev_month':prev_month,
+        'next_month':next_month
     }
     return render(request, 'calendar/calendar.html',context=context)
 
-
-
 def event(request,event_id):
-    print(event_id)
     return render(request, 'calendar/event_list.html')
 
+def get_all_events(dt:date):
+    day_events=Event.objects.all().filter(dt=dt).order_by('tm')
+    day_events_dict
+    for event in day_events:
+
+    return day_events
+
+def days_to_events(month:list[list[date]])->dict[date:list[Event]]:
+    events_list=[]
+    for week in month:
+        week_events=[]
+        for day in week:
+            day_dict=dict()
+            day_dict['day']=day
+            day_dict['weekday']=int_to_weekday(day.weekday())
+            day_dict['events']=get_all_events(day)
+            week_events.append(day_dict)
+        events_list.append(week_events)
+    print(events_list)
+    return events_list
 
 def add_event(request):
     req=request
     print('hiii')
 
-
-
-def calendar_builder(year:int=None,month:int=None)->dict:
-    print(year)
-    print(month)
+def calendar_builder(year:int=None,month:int=None)->list[list[date]]:
     start_day=date(year,month,1)
+    if month==12:
+        year=year+1
+        month=1
     end_day=date(year,month+1,1) - timedelta(days=1)
     start_day=start_day-timedelta(days=start_day.weekday())
     month=[]
@@ -58,3 +93,20 @@ def calendar_builder(year:int=None,month:int=None)->dict:
         start_day+=timedelta(days=1)
         index+=1
     return month
+
+def int_to_weekday(weekday:int):
+    if weekday==0:
+        weekday='Понедельник'
+    elif weekday==1:
+        weekday='Вторник'
+    elif weekday==2:
+        weekday='Среда'
+    elif weekday==3:
+        weekday='Четверг'
+    elif weekday==4:
+        weekday='Пятница'
+    elif weekday==5:
+        weekday='Суббота'
+    elif weekday==6:
+        weekday='Воскресенье'
+    return weekday
